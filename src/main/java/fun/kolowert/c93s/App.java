b5b941e21ab3bccf44b69c04b16f651486c399d0
Taras_Kohut <tiarkey@yahoo.com>
2022-03-28 15:08:33 +0300

    initial

---
+++ src/main/java/fun/kolowert/c93s/App.java
@@ -0,0 +1,10 @@
+package fun.kolowert.c93s;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+
+	}
+
+}
---
+++ src/test/java/fun/kolowert/c93s/C93sApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.kolowert.c93s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C93sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
