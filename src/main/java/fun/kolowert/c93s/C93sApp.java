397613240e528348a4597a2697ede39af9a4afb8
Taras_Kohut <tiarkey@yahoo.com>
2022-04-01 14:18:09 +0300

    initial plus

---
+++ src/main/java/fun/kolowert/c93s/C93sApp.java
@@ -0,0 +1,13 @@
+package fun.kolowert.c93s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C93sApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C93sApp.class, args);
+	}
+
+}
